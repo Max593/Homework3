32ba0b15ca3fb0d85fe8d50d78374a13fa194045
max <maxmolotov@hotmail.it>
2017-04-08 10:54:06 +0200

    Initial commit

---
+++ src/NoSuchNodeException.java
@@ -0,0 +1,5 @@
+/**
+ * Created by max on 08/04/17.
+ */
+public class NoSuchNodeException extends Exception {
+}
---
+++ src/NoSuchPathException.java
@@ -0,0 +1,5 @@
+/**
+ * Created by max on 08/04/17.
+ */
+public class NoSuchPathException extends Exception {
+}
