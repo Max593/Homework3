68010d53d8e9e5ff4c1bf18c6562807b0998034c
Max593 <maxmolotov@hotmail.it>
2017-04-08 18:00:59 +0200

    Piccole correzioni.

--- src/MyNetwork.java
+++ src/MyNetwork.java
@@ -30,17 +30,17 @@ public class MyNetwork <T> implements Network <T> {
     }
 
     @Override
-    public void addNode(T v) {
-
-    }
+    public void addNode(T v) { V.add(v); }
 
     @Override
     public void addEdge(T p, T a) throws NoSuchNodeException {
+        if(!V.contains(p) || !V.contains(a)) { throw new NoSuchNodeException(); }
 
     }
 
     @Override
     public List<T> shortestPath() throws NoSuchPathException {
+        if(source == null || target == null) { throw new NoSuchPathException(); }  //Manca una condizione da inserire dopo
         return null;
     }
 }
