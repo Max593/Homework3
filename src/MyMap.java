c9c8e4ee1c182edd479054720e4e3157cc31ef86
Max593 <maxmolotov@hotmail.it>
2017-04-09 12:58:36 +0200

    Specifica in MyMap.

--- src/MyMap.java
+++ src/MyMap.java
@@ -6,7 +6,7 @@ import java.util.List;
  * Created by max on 09/04/2017.
  */
 public class MyMap <T> extends HashMap <T, List<T>> {
-    public void add(T key, T val) {
+    public void add(T key, T val) {  //Invece di sostituire l'elemento lo aggiunge alla lista associata
         List<T> current = get(key);
         if (current == null) {
             current = new ArrayList<>();
